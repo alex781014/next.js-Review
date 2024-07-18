e8629a91473b35e6fdf583403c3857ec965390a6
alexhu20221003 <https://github.com/alexHu20221003>
2024-07-18 16:41:13 +0800

    test:test commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import '../styles/globals.css';
-import Layout from '../components/layout/layout';
+import "../styles/globals.css";
+import Layout from "../components/layout/layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <h1>Test</h1>
       <Component {...pageProps} />
     </Layout>
   );
